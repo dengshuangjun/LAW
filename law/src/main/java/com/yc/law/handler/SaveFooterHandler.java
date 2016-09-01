b80a5a19e384f99f36287ba8388ae64d93f63bbd
ljy690 <657513865@qq.com>
2016-09-01 21:52:54 +0800

    廖瑾芸：尾部信息修改 ，前台后台，不过通过URI传的中文有乱码现象，需要设置一下tomcat的server.xml

--- law/src/main/java/com/yc/law/handler/SaveFooterHandler.java
+++ law/src/main/java/com/yc/law/handler/SaveFooterHandler.java
@@ -1,26 +1,40 @@
 package com.yc.law.handler;
 
 import java.io.PrintWriter;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.gson.Gson;
 import com.yc.law.entity.Footer;
+import com.yc.law.listener.ServletContextListenerImpl;
 import com.yc.law.util.FooterDomXml;
 
 @Controller
 @RequestMapping("/footer")
 public class SaveFooterHandler {
-	
 	//响应json数据
 	@RequestMapping("/findInfo")
 	public void saveInfo(PrintWriter out){
 		FooterDomXml fdx = new FooterDomXml();
 		Footer footer = fdx.getFootInfo();
-		Gson gson = new Gson();
-		out.println(gson.toJson(footer));
+		JSONArray json=JSONArray.fromObject(footer);//将集合数据变成json集合
+		JSONObject jb=new JSONObject();
+		jb.put("rows", json);
+		out.print(jb);
+		out.flush();
+		out.close();
+	}
+	
+	@RequestMapping("/updateFooter")
+	public void updateFooter(@RequestParam("info") String info,@RequestParam("email") String email,@RequestParam("phone") String phone,PrintWriter out){
+		FooterDomXml fdx = new FooterDomXml();
+		Footer footer = new Footer(info, phone, email);
+		fdx.update(footer, ServletContextListenerImpl.footerXmlPath);
+		out.print(true);
 		out.flush();
 		out.close();
 	}
