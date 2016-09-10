d48b7edfbe6c4eabcfce4e320633bfafc1abfdb0
dsj <862454410@qq.com>
2016-09-10 08:32:58 +0800

    法律类型的功能完善，关于xml动态加载样式在后台解析，不会造成之前没有样式页面出现--邓双军

--- law/src/main/java/com/yc/law/handler/BackDataHandler.java
+++ law/src/main/java/com/yc/law/handler/BackDataHandler.java
@@ -1,6 +1,8 @@
 package com.yc.law.handler;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yc.law.entity.Type;
 import com.yc.law.entity.TypePage;
 import com.yc.law.service.BackDataService;
 
@@ -25,5 +28,43 @@ public class BackDataHandler {
 		map.put("total", typePage.getTotal());
 		map.put("rows", typePage.getTypeList());
 		return map;
+	}
+	@RequestMapping("addTypes")
+	@ResponseBody
+	public boolean addTypes(Type type){
+		System.out.println(type);
+		return backDataService.addTypes(type);
+	}
+	@RequestMapping("delTypes")
+	@ResponseBody
+	public boolean delTypes(String ntid){
+		
+		List<Integer> list=new ArrayList<Integer>();
+		if(ntid==null){
+			return false;
+		}else if(ntid.contains(",")){
+			String[] result = ntid.split(",");
+			for(int i=0,len=result.length;i<len;i++){
+				list.add(Integer.parseInt(result[i]));
+			}
+		}else{
+			list.add(Integer.parseInt(ntid));
+		}
+
+		try {
+			if(backDataService.delTypes(list)>0){
+				return true;
+			}else{
+				return false;
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		
+	}
+	@RequestMapping("/updateType")
+	@ResponseBody
+	public boolean updateType(Type type){//Type [ntid=1025, ntname=民事, status=N, usid=1002, usname=admin, note=]
+		return backDataService.updateType(type);
 	}
 }
